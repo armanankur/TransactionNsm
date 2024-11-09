b83122ed1ea6fd1590e00b737a13fa9be3c2ccce
Ankur Anand <93547910+armanankur@users.noreply.github.com>
2024-11-09 17:01:23 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+//import './App.css'
+import LatestTransaction from './components/component/Transaction'
+
+function App() {
+
+
+  return (
+    <div>
+     <LatestTransaction/>
+    </div>
+  )
+}
+
+export default App
