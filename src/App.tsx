8c49d77f371244589e18fa8bde58a38f0a901230
Ankur Anand <armanankur@gmail.com>
2024-11-12 14:04:03 +0530

    changesinApp

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+//import './App.css'
+import LatestTransaction from './components/component/LatestTransaction'
+
+function App() {
+
+
+  return (
+    <div>
+     <LatestTransaction/>
+    </div>
+  )
+}
+
+export default App
